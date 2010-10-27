5ae4d0be8a7fde40216cd1f4849e70b440b31c24
Kerry Buckley <kerryjbuckley@gmail.com>
2010-10-27 11:59:19 +0100

    Tart up form

--- javascripts/dom_selector.js
+++ javascripts/dom_selector.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
   $("#selector").focus();
   highlighted_selector = ""
-  $("#selector").keyup(function() {
-    $(highlighted_selector).css("background-color", "");
-    highlighted_selector = $("#selector").val();
-    try {
-      $(highlighted_selector).css("background-color", "#ccf");
-    } catch (error) {
-      highlighted_selector = ""
-    }
-  });
+  $("#selector").keyup(highlightSelected);
 });
+
+function highlightSelected() {
+  $(highlighted_selector).css("background-color", "");
+  highlighted_selector = $("#selector").val();
+  try {
+    $(highlighted_selector).css("background-color", "#ccf");
+  } catch (error) {
+    highlighted_selector = ""
+  }
+}
---
+++ javascripts/jquery_masterclass.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  roundPanelCorners();
+});
+
+function roundPanelCorners() {
+  $(".panel").corner();
+}
