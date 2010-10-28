a40f524466c625d6d5e3e0842e65314d71057db1
Kerry Buckley <kerryjbuckley@gmail.com>
2010-10-28 21:13:16 +0100

    OK, that was working by coincidence. Let's keep it simple.

--- javascripts/dom_selector.js
+++ javascripts/dom_selector.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
   $("#selector").focus();
   $("#selector").keyup(highlightSelected);
-  function removePreviousHighlighting() {}
 });
 
 function highlightSelected() {
-  try {
-    removePreviousHighlighting();
-  } catch (error) {
-  }
-  highlighted_selector = $("#selector").val();
+  $("*").removeClass("selected");
+  var highlighted_selector = $("#selector").val();
   try {
     $(highlighted_selector).addClass("selected");
   } catch (error) {
-  }
-  function removePreviousHighlighting() {
-    $(highlighted_selector).removeClass("selected");
   }
 }
